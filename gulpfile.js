2cb09fb73821969a75a429df448a54e9b94284a7
Viacheslav Shestakov <slavassh@gmail.com>
2016-10-18 17:05:03 +0500

    Правильное расположение для style.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-var gulp        = require('gulp');
-var plumber     = require('gulp-plumber');
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var syntax      = require('postcss-scss');
-var postcss     = require('gulp-postcss');
-var sorting     = require('postcss-sorting');
-var prefix      = require('autoprefixer');
-var cp          = require('child_process');
+var sass = require('gulp-sass');
+var syntax = require('postcss-scss');
+var postcss = require('gulp-postcss');
+var sorting = require('postcss-sorting');
+var prefix = require('autoprefixer');
+var cp = require('child_process');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
     jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
@@ -18,7 +18,7 @@ var messages = {
  */
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+    return cp.spawn(jekyll, ['build'], { stdio: 'inherit' })
         .on('close', done);
 });
 
@@ -32,7 +32,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 /**
  * Wait for jekyll-build, then launch the Server
  */
-gulp.task('browser-sync', ['style', 'jekyll-build'], function() {
+gulp.task('browser-sync', ['style', 'jekyll-build'], function () {
     browserSync({
         server: {
             baseDir: '_site'
@@ -44,28 +44,32 @@ gulp.task('browser-sync', ['style', 'jekyll-build'], function() {
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
 gulp.task('style', function () {
-    return gulp.src('_scss/style.scss')
+    return gulp.src('style.scss')
         .pipe(plumber())
         .pipe(postcss([sorting(
-            { 	"sort-order": "alphabetical",
+            {
+                "sort-order": "alphabetical",
                 "empty-lines-between-children-rules": 1,
-                "empty-lines-between-media-rules": 1 }
-            )], {syntax: syntax}))
+                "empty-lines-between-media-rules": 1
+            }
+        )], { syntax: syntax }))
         .pipe(sass({
             includePaths: ['_scss'],
             onError: browserSync.notify
         }))
         .pipe(postcss([
-          prefix({browsers: [
-            "last 1 version",
-            "last 2 Chrome versions",
-            "last 2 Firefox versions",
-            "last 2 Opera versions",
-            "last 2 Edge versions"
-          ]})
-        ]))        
+            prefix({
+                browsers: [
+                    "last 1 version",
+                    "last 2 Chrome versions",
+                    "last 2 Firefox versions",
+                    "last 2 Opera versions",
+                    "last 2 Edge versions"
+                ]
+            })
+        ]))
         .pipe(gulp.dest('_site/css'))
-        .pipe(browserSync.reload({stream:true}))
+        .pipe(browserSync.reload({ stream: true }))
         .pipe(gulp.dest('css'));
 });
 
@@ -74,7 +78,7 @@ gulp.task('style', function () {
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('_scss/*.scss', ['style']);
+    gulp.watch(['*.scss', '_scss/*.scss'], ['style']);
     gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
 
